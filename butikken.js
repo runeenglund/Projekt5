b03e9f215972f3236f3acb9204356a7547370c68
Mtakat01 <96202664+Mtakat01@users.noreply.github.com>
2022-04-20 10:44:36 +0200

    rettelser og hover

--- butikken.js
+++ butikken.js
@@ -1,9 +1,8 @@
-/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-function myFunction() {
-  var x = document.getElementById("myLinks");
-  if (x.style.display === "block") {
-    x.style.display = "none";
-  } else {
-    x.style.display = "block";
-  }
+
+var navlinks = document.getElementById("nav");
+function showmenu(){
+  navlinks.style.right= "0";
+}
+function hidemenu(){
+  navlinks.style.right= "-200px";
 }
